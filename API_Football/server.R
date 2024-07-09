fd627ee38f56a9f8281d3e0c411d4100f3a71e41
Xingwang Yu <kevin.xwyu@gmail.com>
2024-07-09 14:46:29 -0400

    message

--- API_Football/server.R
+++ API_Football/server.R
@@ -1,28 +1,39 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
 
-library(shiny)
 
-# Define server logic required to draw a histogram
-function(input, output, session) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
-    })
-
+server <- function(input, output, session) {
+  observeEvent(input$update, {
+    endpoint <- input$endpoint
+    league <- input$league
+    season <- input$season
+    team <- input$team
+   
+    if (endpoint == "players/topscorers") {
+      # Fetch Top Scorers Data
+      top_scorers_data <- get_football_data(endpoint, list(league = league, season = season))
+      top_scorers_list <- as_tibble(top_scorers_data$response$player)
+      top_scorers_stats <- bind_rows(lapply(top_scorers_data$response$statistics, as_tibble, .name_repair="unique"))
+      top_scorers_df <- bind_cols(top_scorers_list, top_scorers_stats)
+      
+      # subset data
+      filtered_scorers <- top_scorers_df |> filter(goals$total >= input$min_goals)
+      output$top_scorers_table <- renderDataTable(filtered_scorers)
+      
+      # Plot Top Scorers Data
+      output$topScorersPlot <- renderPlot({
+        ggplot(filtered_scorers, aes_string(x = "name", y = input$varSelection)) +
+          geom_bar(stat = "identity", fill = "skyblue") +
+          coord_flip() +
+          theme_minimal() +
+          labs(x = "Player Name")
+        })
+    } else if (endpoint == "teams/statistics") {
+      #fetch team stats data
+      team_data <- get_football_data(endpoint, list(league = league, season = season, team = team))
+      team_fixture <- as_tibble(team_data$response$fixtures)
+      output$team_fixture_table <- renderDataTable(team_fixture)
+     
+      
+    } 
+    
+  })
 }
--- API_Football/ui.R
+++ API_Football/ui.R
@@ -8,27 +8,89 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 library(ggplot2)
+library(shinydashboard)
 
-#
-# Define UI for application that draws a histogram
-fluidPage(
+# Define UI
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+header <- dashboardHeader(title = "Football Data Dashboard")
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+siderbar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("About", tabName = "about"),
+    menuItem("Data_Download", tabName = "data_download"),
+    menuItem("Data_Exploration", tabName = "data_exploration")
+  )
+)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
-        )
+body <- dashboardBody(
+    tabItems(
+      tabItem(tabName = "about",
+              fluidRow(
+                box(title = "This App", status = "primary", solidHeader = TRUE,
+                    "AAAA"
+                )
+              ),
+              
+              fluidRow(
+                box(title = "Data Source", status = "primary", solidHeader = TRUE,
+                    "BBBB"
+                ),
+                box(title = "Each Tap", status = "primary", solidHeader = TRUE,
+                    "CCCC"
+                )
+              )
+      ),
+      
+      tabItem(tabName = "data_download",
+              fluidRow(
+                box(title = "Endpoint", status = "primary", solidHeader = TRUE,
+                    selectInput("endpoint", "Select endpoint", choices = c("players/topscorers", "teams/statistics"))),
+             
+                box(title = "Parameter Input", status = "primary", solidHeader = TRUE,
+                    textInput("league", "League", value = "39"),
+                    textInput("season", "Season", value = "2022"),
+                    conditionalPanel(condition = "input.endpoint == 'teams/statistics'",
+                                     textInput("team", "Team", value = "33")),
+                    conditionalPanel(condition = "input.endpoint == 'players/topscorers'",
+                                    sliderInput("min_goals", "Minimum Goals", min = 0, max = 30, value = 0)),
+                    actionButton("update", "Update Data")
+                    )
+                ),
+              
+              fluidRow(
+                box(title = "Data overview", status = "primary", solidHeader = TRUE,
+                    conditionalPanel(condition = "input.endpoint == 'players/topscorers'",
+                                     dataTableOutput("top_scorers_table"))
+                    )
+                )
+              ),
+              
+      
+      tabItem(tabName = "data_exploration", 
+              fluidRow(
+                conditionalPanel(condition = "input.endpoint == 'players/topscorers'",
+                  box(title = "Shot and goal summary of Top Scorers", 
+                    selectInput("varSelection", "Variables", choices = c("Total shots"="shots$total", "Shots On"="shots$on", "Total goals"="goals$total")),
+                    plotOutput("topScorersPlot", height = 350)
+                )
+               )
+              ),
+              
+              fluidRow(
+                box(title = "EE", status = "primary", solidHeader = TRUE,
+                    "BBBB"
+                ),
+                box(title = "RR", status = "primary", solidHeader = TRUE,
+                    "CCCC"
+                )
+              )
+      )
     )
 )
+
+ui <- dashboardPage(header, siderbar, body)
+  
+  
+  
+  
+    
